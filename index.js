801ee7214eb95077e9f95b23e2871b463b32bc10
paula <paula_ribe93@hotmail.com>
2023-04-18 16:36:54 +0200

    feat:Model creation and migration of role table

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.use(express.json());
 app.listen(PORT, () =>{
     console.log(`Server up and running on port ${PORT}`);
 
-    // db.authenticate()
-    // .then(() => {
-    //     console.log("Connected to the database, sync is ok");
-    // })
-    // .catch((error) => {
-    //     console.log("Error: " + error);
-    // });
+    db.authenticate()
+    .then(() => {
+        console.log("Connected to the database, sync is ok");
+    })
+    .catch((error) => {
+        console.log("Error: " + error);
+    });
 })
