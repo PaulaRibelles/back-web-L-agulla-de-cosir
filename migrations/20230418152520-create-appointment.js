c9f5365a15e1562a313dfc9409351579ce083a53
paula <paula_ribe93@hotmail.com>
2023-04-18 17:40:26 +0200

    feat: Model creation and migration of appointment table with FK

---
+++ migrations/20230418152520-create-appointment.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Appointments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      client_id: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: "Clients",
+          key: "id"
+        }
+      },
+      dressmaker_id: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: "Dressmakers",
+          key:"id"
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Appointments');
+  }
+};
