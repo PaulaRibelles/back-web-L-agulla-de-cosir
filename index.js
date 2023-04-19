5789e26ace58fbe12bc14b76dedd97d9966710a7
paula <paula_ribe93@hotmail.com>
2023-04-19 19:04:31 +0200

    feat: Controller and route User created

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const db = require("./db/db");
+
+require('dotenv').config();
 
 const PORT = 5000;
 
 app.use(express.json());
+app.use(userRoutes);
 
 app.listen(PORT, () =>{
     console.log(`Server up and running on port ${PORT}`);
---
+++ views/userRoutes.js
@@ -0,0 +1,6 @@
+const userController =require('../controllers/userController');
+const router = require('express').Router();
+
+
+
+module.exports = router;
