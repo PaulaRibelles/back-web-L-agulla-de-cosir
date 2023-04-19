c64fa66960059c1ccb08f832162bb9f507690d89
paula <paula_ribe93@hotmail.com>
2023-04-19 19:30:22 +0200

    feat: Creating User endpoint and routes; profile and update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,54 @@
 const { User } = require("../models")
+const bcrypt = require('bcrypt');
 
 const userController = {};
 
+userController.profile = async(req, res) => {
+    try {
+        const userId = req.userId;
+        const user = await User.findByPk(userId)
 
+        return res.json(user);
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+userController.updateUser = async (req, res) => {
+    try {
+        const { name, password, surname, city, phone, email } = req.body;
+        const userId = req.userId;
+
+        let updateFields = {
+            name: name,
+            surname: surname,
+            city: city,
+            phone: phone,
+            email: email,
+        };
+
+        if (password) {
+            const encryptedPassword = bcrypt.hashSync(password, 10);
+            updateFields.password = encryptedPassword;
+        }
+
+        const updateUser = await User.update(
+            updateFields,
+            {
+                where: {
+                    id: userId
+                }
+            }
+        );
+
+        if (!updateUser) {
+            return res.send('User not updated')
+        }
+
+        return res.send('User updated')
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
 
 module.exports = userController;
--- views/userRoutes.js
+++ views/userRoutes.js
@@ -1,6 +1,9 @@
 const userController =require('../controllers/userController');
 const router = require('express').Router();
 
+//ENDPOINTS
 
+router.get('/profile/', userController.profile)
+router.put('/update/', userController.updateUser)
 
 module.exports = router;
