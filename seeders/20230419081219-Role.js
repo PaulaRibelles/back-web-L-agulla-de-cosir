829f2cb5c85b260bfc03ab0c0375f74238fa86ce
paula <paula_ribe93@hotmail.com>
2023-04-19 13:13:12 +0200

    fix: Correction spelling mistake in the role seeder

--- seeders/20230419081219-Role.js
+++ seeders/20230419081219-Role.js
@@ -9,13 +9,13 @@ module.exports = {
           id: 1,
           type: "User",
           createdAt: "2023-03-01 00:00:00",
-          updateAt: "2023-03-01 00:00:00"
+          updatedAt: "2023-03-01 00:00:00"
         },
         {
           id: 2,
           type: "Admin",
           createdAt: "2023-03-01 00:00:00",
-          updateAt: "2023-03-01 00:00:00" 
+          updatedAt: "2023-03-01 00:00:00" 
         }
     ], {});
     
