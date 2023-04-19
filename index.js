5ed28c6a155eb8a566d96d13b8ba93486fc26359
paula <paula_ribe93@hotmail.com>
2023-04-19 20:24:45 +0200

    fix: Changes to index.js on User route

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const app = express();
 const db = require("./db/db");
+const router = require('./router');
 
 require('dotenv').config();
 
 const PORT = 5000;
 
 app.use(express.json());
-app.use(userRoutes);
+app.use(router);
 
-// app.listen(PORT, () =>{
-//     console.log(`Server up and running on port ${PORT}`);
+app.listen(PORT, () =>{
+    console.log(`Server up and running on port ${PORT}`);
 
-//     db.authenticate()
-//     .then(() => {
-//         console.log("Connected to the database, sync is ok");
-//     })
-//     .catch((error) => {
-//         console.log("Error: " + error);
-//     });
-// })
+    db.authenticate()
+    .then(() => {
+        console.log("Connected to the database, sync is ok");
+    })
+    .catch((error) => {
+        console.log("Error: " + error);
+    });
+})
